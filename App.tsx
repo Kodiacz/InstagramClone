11403c17ee4b99c8e179fd586f876969441e9946
Simeon Yordanov <sjordanov@code.bg>
2023-10-09 17:09:34 +0300

    minor changes
    forgot to add initial commit so this combines both the inital and the new changes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
+import * as firebaseApp from 'firebase/app'
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDyi97YMBClZDnl5IwV-73rehyfV3mUYk",
+  authDomain: "instagramclone-9f4c2.firebaseapp.com",
+  projectId: "instagramclone-9f4c2",
+  storageBucket: "instagramclone-9f4c2.appspot.com",
+  messagingSenderId: "278613975270",
+  appId: "1:278613975270:web:2e93625d2a7284d50e1b27",
+  measurementId: "G-E81QMK49B8"
+};
+
+if (firebaseApp.getApps().length === 0) {
+  console.log(firebaseApp.initializeApp(firebaseConfig))
+}
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer, NavigationProp, ParamListBase } from '@react-navigation/native'
+import { default as LandingScreen } from './components/auth/Landing';
+import { Register as RegisterScreen } from './components/auth/Register';
+import { Login as LoginScreen } from './components/auth/Login';
+
+type MainStackParamList = {
+  Landing: { navigation: NavigationProp<ParamListBase> },
+  Register: {},
+  Login: {},
+}
+
+const Stack = createStackNavigator<MainStackParamList>()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Landing'>
+        <Stack.Screen name='Landing' component={LandingScreen} options={{ headerShown: false }} />
+        <Stack.Screen name='Register' component={RegisterScreen} />
+        <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+} 
