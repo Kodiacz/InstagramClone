849b882b10fcc033ba6e7a89a3559b6f116e0bbd
Simeon Yordanov <sjordanov@code.bg>
2023-11-12 00:53:17 +0200

    created Post type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,14 @@
+import { Timespan } from 'react-native/Libraries/Utilities/IPerformanceLogger';
+
 type User = {
 	name: string;
 	email: string;
 };
+
+type Post = {
+	caption: string;
+	creation: Timespan;
+	downloadURL: string;
+};
+
+export { User, Post };
