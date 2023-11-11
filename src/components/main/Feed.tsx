b7e13857db87f0ca5794b7fc52bde7924d8f0010
Simeon Yordanov <sjordanov@code.bg>
2023-11-11 11:26:37 +0200

    created Feed screen

--- src/components/main/Feed.tsx
+++ src/components/main/Feed.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import useSafeArea from '../../custom-hooks/useSafeView';
 
 export default function Feed() {
+	const { safeArea } = useSafeArea();
+
+	console.log(safeArea);
+
 	return (
-		<View>
+		<View style={safeArea}>
 			<Text>Feed</Text>
 		</View>
 	);
