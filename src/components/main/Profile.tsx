fca58809a094b6ba231a84017579e608714525ab
Simeon Yordanov <sjordanov@code.bg>
2023-11-11 11:27:49 +0200

    created profile screen

--- src/components/main/Profile.tsx
+++ src/components/main/Profile.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import useSafeArea from '../../custom-hooks/useSafeView';
+import { connect } from 'react-redux';
 
-export default function Profile() {
+function Profile() {
+	const { safeArea } = useSafeArea();
+
+	console.log(safeArea);
+
 	return (
-		<View>
+		<View style={safeArea}>
 			<Text>Profile</Text>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({});
+
+const mapStateToProps = (store: any) => ({
+	currentUser: store.userState.currentUser,
+	posts: store.userState.posts,
+});
+
+export default connect(mapStateToProps, null)(Profile);
